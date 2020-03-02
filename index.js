e48ea30737e01397f283f964d207c25816fecc35
Katie Seggelke <katie.seggelke@gmail.com>
2020-03-02 20:33:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+  for (let i = 0; i < names.length; i++){
+    console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+  }
+  
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");

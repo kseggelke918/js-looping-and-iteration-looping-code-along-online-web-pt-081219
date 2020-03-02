9e32f6c6527d52f1b05c8a486a4011d86be8153e
Katie Seggelke <katie.seggelke@gmail.com>
2020-03-02 20:35:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(namesArray, event){
-  for (let i = 0; i < names.length; i++){
+  for (let i = 0; i < namesArray.length; i++){
     console.log(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
   }
   
